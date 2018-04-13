6ec36a8be827b5f1435e7d1eadf01cabb8117803
Zhaowenhor <wenhora@gmail.com>
2018-04-13 22:36:49 +0800

    dev 分支的创建!

--- java/MyTest.java
+++ java/MyTest.java
@@ -6,7 +6,7 @@ public class MyTest {
     public void HtppClient(){
         //刚才少了一个readme文件，导致无法记录
         System.out.println("本次进行分支合并!");
+        System.out.println("第二次合并!");
     }
-        //测试
-
-}
+ }
+    
